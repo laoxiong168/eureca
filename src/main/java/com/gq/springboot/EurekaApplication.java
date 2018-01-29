c7364b09c5c953edb3501f1d091ba60c1d56b32b
xzy <xzy@xzy-PC.localdomain>
2018-01-29 17:29:57 +0800

    spring boot 注册中心

---
+++ src/main/java/com/gq/springboot/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.gq.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+		System.out.println("注册中心已启动...");
+	}
+}
